7ae86c7f9d5ee871871206807eb8a83175b496b6
Norman <pescado@crudo.com>
2022-11-24 08:53:33 -0600

    fixed dark overlay

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 let sideMenu = document.querySelector('.side-menu')
+let wideCont = document.querySelector('.wide-cont')
 sideMenu.style.right = -sideMenu.clientWidth + 'px';
 sideMenu.style.transition = '0.2s'
 // sideMenu.style.right = 0 + 'px';
@@ -9,4 +10,10 @@ let toggleIcon = (n) => {
     }else{
         sideMenu.style.right = -sideMenu.clientWidth + 'px';
     }
+
+    displayDarkBg();
+}
+
+let displayDarkBg = () => {
+    wideCont.classList.toggle('dark-overlay');
 }
